7fd692d13a73f3852bc60690b7c196bd467e12e9
BananaBoii <klnaveen13@gmail.com>
2022-03-31 12:13:26 +0530

    Source Code

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from tkinter import ttk
 root = Tk()
 root.title("Translator")
 root.geometry("880x300")
+root.resizable(False, False)
 
 #Functions
 def translate():
@@ -42,6 +43,31 @@ def clear():
     translated_text.delete(1.0, END)
     translated_text.configure(state="disabled")
 
+def Dark_Mode():
+    Main_Color = "black"
+    Second_Color = "gray"
+    Text_Color = "white"
+    original_text.config(bg=Main_Color, fg=Text_Color)
+    translated_text.config(bg=Main_Color, fg=Text_Color)
+    root.config(bg=Second_Color)
+    translate_button.config(bg=Main_Color, fg=Text_Color)
+    dark_button.config(bg=Main_Color, fg=Text_Color)
+    clear_button.config(bg=Main_Color, fg=Text_Color)
+    light_button.config(bg=Main_Color, fg=Text_Color)
+
+def Light_Mode():
+    Main_Color = "SystemButtonFace"
+    Second_Color = "SystemButtonFace"
+    Text_Color = "black"
+    original_text.config(bg="white", fg=Text_Color)
+    translated_text.config(bg="white", fg=Text_Color)
+    root.config(bg=Second_Color)
+    translate_button.config(bg=Main_Color, fg=Text_Color)
+    dark_button.config(bg=Main_Color, fg=Text_Color)
+    clear_button.config(bg=Main_Color, fg=Text_Color)
+    light_button.config(bg=Main_Color, fg=Text_Color)
+
+
 languages = googletrans.LANGUAGES
 
 language_list = list(languages.values())
@@ -57,6 +83,7 @@ original_text.grid(row=0, column=0, padx=10, pady=20)
 translate_button = Button(root, text="Translate", font=("Helvetica", 24), command=translate)
 translate_button.grid(row=0, column=1, padx=10)
 
+#Text Boxes
 translated_text = Text(root, height=10, width=40)
 translated_text.grid(row=0, column=2, padx=10, pady=20)
 
@@ -71,9 +98,15 @@ translated_combo.current(26)
 translated_combo.grid(row=1, column=2)
 
 
-#Clear button
+#Buttons
 clear_button = Button(root, text="Clear", command=clear)
-clear_button.grid(row=2, column=1)
+clear_button.grid(row=2, column=1, pady=10)
+
+dark_button = Button(root, text="Dark Mode", command=Dark_Mode)
+dark_button.grid(row=2, column=2, pady=10)
+
+light_button = Button(root, text="Light Mode", command=Light_Mode)
+light_button.grid(row=2, column=0, pady=10)
 
 
 root.mainloop()
